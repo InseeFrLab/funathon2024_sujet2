dbc5492fa577d8bd3c3142ee54bab12a604a5473
Pierre <pierre.girard34@gmail.com>
2024-02-12 14:04:26 +0100

    install package DT if necessary

--- global.R
+++ global.R
@@ -1,11 +1,6 @@
 # RTraffic global
 
-#install.packages("DT")#if necessary install package
-
-#Test Push Thomas1
-#Test Push Thomas2
-
-#rm(list = ls())
+if (!requireNamespace("DT")){install.packages("DT")} #install.packages("DT")#if necessary install package
 t0 = Sys.time()
 if (exists("data_already_loaded")){#check the data is already loaded----
   print("already loaded")} else {
@@ -19,24 +14,23 @@ if (exists("data_already_loaded")){#check the data is already loaded----
                          "https://www.data.gouv.fr/fr/datasets/r/6717f107-be00-4b4b-9706-fa0e5190fb69",
                          "https://www.data.gouv.fr/fr/datasets/r/2f9f6e54-e2d7-4e85-b811-2e5e68fa5bca",
                          "https://www.data.gouv.fr/fr/datasets/r/f1bd931e-c99e-41ce-865e-9e9785c903ec"
-                       ),#apt airport data year after year
+                       ),#apt airport data
                        c("https://www.data.gouv.fr/fr/datasets/r/9c5354ad-31cb-4217-bc88-fb7c9be22655",
                          "https://www.data.gouv.fr/fr/datasets/r/0c0a451e-983b-4f06-9627-b5ff1bccd2fc",
                          "https://www.data.gouv.fr/fr/datasets/r/dad30bed-7276-4a67-a1ab-a856e6e01788",
                          "https://www.data.gouv.fr/fr/datasets/r/bbf6492d-86ac-43a0-9260-7df2ffdb5a77",
                          "https://www.data.gouv.fr/fr/datasets/r/af8950bc-e90a-4b7e-bb81-70c79d4c3846"
-                       ),#lsn links data year after year
+                       ),#lsn link data
                        c("https://www.data.gouv.fr/fr/datasets/r/ddfea6a0-df7e-4402-99fc-165f573f2e10",
                          "https://www.data.gouv.fr/fr/datasets/r/8421e029-c8c7-410d-b38c-54455ac3265d",
                          "https://www.data.gouv.fr/fr/datasets/r/818eec10-6122-4788-8233-482e779ab837",
                          "https://www.data.gouv.fr/fr/datasets/r/0b954774-ccd1-43ec-9b5a-f958fba03e87",
                          "https://www.data.gouv.fr/fr/datasets/r/bcec3e1e-940a-4772-bc28-0d7b2b53c718"
-                       )#cie company data year after year
+                       )#cie company data
     )
     names(paths) = c("year","apt","lsn","cie")
     
     #load libraries & variables & functions----
-    #library(data.table)
     library(dplyr)
     library(shiny)
     library(stringr)
