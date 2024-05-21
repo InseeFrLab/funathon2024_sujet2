d70a162dff5f58576f3eca29dbbdf7aaefdc1bf5
lgaliana <lino.galiana@insee.fr>
2024-05-21 12:42:47 +0000

    Update environment

--- R/load_data.R
+++ R/load_data.R
@@ -3,6 +3,7 @@ library(yaml)
 library(readr)
 library(janitor)
 library(dplyr)
+library(stringr)
 
 create_data_list <- function(source_file = "sources.yml"){
   catalogue <- yaml::read_yaml(source_file)
