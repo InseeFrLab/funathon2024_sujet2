db433f7260161bbc8a0eb19a170c1c3738bb8e73
linogaliana <lino.galiana@insee.fr>
2024-06-14 16:06:40 +0000

    exo2

--- intermediates/exo2.py
+++ intermediates/exo2.py
@@ -1,15 +1,22 @@
+import pandas as pd
 import geopandas as gpd
 
 import src.import_data as sid
 from src.create_data_list import create_data_list
 
 # Load data ----------------------------------
-urls <- create_data_list("./sources.yml")
+urls = create_data_list("./sources.yml")
 
 
-pax_apt_all = sid.import_airport_data(urls['airports'])
-pax_cie_all = sid.import_compagnies_data(urls['compagnies'])
-pax_lsn_all = sid.import_liaisons_data(urls['liaisons'])
+pax_apt_all = pd.concat(
+    [sid.import_airport_data(l) for l in urls['airports'].values()]
+)
+pax_cie_all = pd.concat(
+    [sid.import_airport_data(l) for l in urls['compagnies'].values()]
+)
+pax_lsn_all = pd.concat(
+    [sid.import_airport_data(l) for l in urls['liaisons'].values()]
+)
 
 airports_location = gpd.read_file(
     urls['geojson']['airport']
