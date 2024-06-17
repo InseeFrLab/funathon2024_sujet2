daf821f1e50223140810adea92607dd74a380fa3
linogaliana <lino.galiana@insee.fr>
2024-06-17 12:03:05 +0000

    corrige typo

--- src/clean_dataframe.py
+++ src/clean_dataframe.py
@@ -1,10 +1,10 @@
 def clean_dataframe(df):
     # Create 'an' and 'mois' columns
-    df['an'] = df['ANMOIS'].str[:4]
-    df['mois'] = df['ANMOIS'].str[-2:]
+    df['an'] = df['ANMOIS'].astype(str).str[:4]
+    df['mois'] = df['ANMOIS'].astype(str).str[-2:]
 
     # Remove leading zeros from 'mois' column
-    df['mois'] = df['mois'].str.replace(r'^0+', '', regex = True)
+    df['mois'] = df['mois'].str.replace(r'^0+', '', regex=True)
 
     # Convert all column names to lowercase
     df.columns = df.columns.str.lower()
