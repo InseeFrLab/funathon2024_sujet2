78dfeacf320305ce2676c182c2ba88c673be3a81
linogaliana <lino.galiana@insee.fr>
2024-06-17 11:25:10 +0000

    Corrige typo

--- src/divers_functions.py
+++ src/divers_functions.py
@@ -1,11 +1,12 @@
 def create_data_from_input(data, year, month):
   data = ( data
-    .iloc[data['mois'] == month & data["an"] == year]
+    .loc[data['mois'].astype(str) == str(month)]
+    .loc[data["an"].astype(str) == str(year)]
   )
   return data
 
 
-def summary_stat_airport(data){
+def summary_stat_airport(data):
     table2 = (
         data
         .groupby("apt", "apt_nom")
