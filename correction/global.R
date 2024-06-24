9dea6ef380c4c14ac8abd965419f9468b08aa9d1
Pierre <pierre.girard34@gmail.com>
2024-06-24 14:47:35 +0200

    correct the WD, otherwise the script doesn't run

--- correction/global.R
+++ correction/global.R
@@ -11,6 +11,7 @@ library(gt)
 library(leaflet)
 library(bslib)
 
+setwd("correction/")#indicates the right WD, otherwise it doesn't run correctly
 source("R/import_data.R")
 source("R/create_data_list.R")
 source("R/clean_dataframe.R")
