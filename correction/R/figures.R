626ddb2f5bcc12649c5a050abaf962f9d41cac90
Julien PRAMIL <100139338+jpramil@users.noreply.github.com>
2024-06-26 14:04:57 +0200

    debug map R

--- correction/R/figures.R
+++ correction/R/figures.R
@@ -26,13 +26,16 @@ map_leaflet_airport <- function(df, airports_location, months, years){
     mutate(
       date = as.Date(paste(anmois, "01", sep=""), format = "%Y%m%d")
     ) %>%
-    filter(mois %in% months, an %in% years)
+    filter(mois == months, an == years)
+  
   trafic_aeroports <- airports_location %>%
+    select(Code.OACI, Nom, geometry) %>% 
     inner_join(trafic_date, by = c("Code.OACI" = "apt"))
   
   
   trafic_aeroports <- trafic_aeroports %>%
     mutate(
+      trafic = apt_pax_dep + apt_pax_tr + apt_pax_arr,
       volume = ntile(trafic, 3)
     ) %>%
     mutate(
@@ -54,3 +57,4 @@ map_leaflet_airport <- function(df, airports_location, months, years){
   
   return(carte_interactive)
 }
+
