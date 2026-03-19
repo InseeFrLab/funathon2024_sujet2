59ba0e7add77debdbb77fa393fa65745cce8f4a9
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2026-03-19 16:45:22 +0000

    update download and zip in interim file #30

--- intermediates/exo3.py
+++ intermediates/exo3.py
@@ -10,9 +10,9 @@ from src.create_data_list import create_data_list
 urls = create_data_list("./sources.yml")
 
 
-pax_apt_all = sid.import_airport_data(urls['airports'].values())
-pax_cie_all = sid.import_airport_data(urls['compagnies'].values())
-pax_lsn_all = sid.import_airport_data(urls['liaisons'].values())
+pax_apt_all = sid.import_airport_data(sid.download_and_unzip(urls['airports']))
+pax_cie_all = sid.import_airport_data(sid.download_and_unzip(urls['compagnies']))
+pax_lsn_all = sid.import_airport_data(sid.download_and_unzip(urls['liaisons']))
 
 
 airports_location = gpd.read_file(
