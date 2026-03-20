a082fa07b2ef4aceabd15486da6d930fd97b075b
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2026-03-20 08:57:24 +0000

    with download zip and import for R #30

--- correction/R/import_data.R
+++ correction/R/import_data.R
@@ -1,7 +1,27 @@
-import_airport_data <- function(list_files){
-  
+download_and_unzip <- function(zip_url, extract_to = "temp") {
+  # Create the directory if it doesn't exist
+  dir.create(extract_to, recursive = TRUE, showWarnings = FALSE)
+
+  # Download the zip file
+  zip_path <- file.path(extract_to, "downloaded_file.zip")
+  request(zip_url) %>%
+    req_perform(path = zip_path)
+
+  # Unzip the file
+  unzip(zip_path, exdir = extract_to)
+
+  # Send back names of all extracted files
+  extracted_files <- file.path(extract_to, unzip(zip_path, list = TRUE)$Name)
+
+  # Clean up the zip file
+  file.remove(zip_path)
+
+  return(extracted_files)
+}
+
+import_airport_data <- function(list_files) {
   pax_apt_all <- readr::read_csv2(
-    list_files, 
+    list_files,
     col_types = cols(
       ANMOIS = col_character(),
       APT = col_character(),
@@ -9,16 +29,13 @@ import_airport_data <- function(list_files){
       APT_ZON = col_character(),
       .default = col_double()
     )
-  ) %>% 
+  ) %>%
     clean_dataframe()
-  
+
   return(pax_apt_all)
-  
 }
 
-
-import_compagnies_data <- function(list_files){
-  
+import_compagnies_data <- function(list_files) {
   pax_cie_all <- readr::read_csv2(
     file = list_files,
     col_types = cols(
@@ -29,17 +46,14 @@ import_compagnies_data <- function(list_files){
       CIE_PAYS = col_character(),
       .default = col_double()
     )
-  ) %>% 
+  ) %>%
     clean_dataframe()
-  
+
   return(pax_cie_all)
-  
-  
 }
 
 
-import_liaisons_data <- function(list_files){
-  
+import_liaisons_data <- function(list_files) {
   pax_lsn_all <- readr::read_csv2(
     file = list_files,
     col_types = cols(
@@ -50,11 +64,9 @@ import_liaisons_data <- function(list_files){
       LSN_SCT = col_character(),
       LSN_FSC = col_character(),
       .default = col_double()
-    ) 
-  ) %>% 
+    )
+  ) %>%
     clean_dataframe()
-  
+
   return(pax_lsn_all)
-  
-  
 }
