66fb70312b1b83023c99831f4634d4d8617b612c
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2026-03-20 11:31:15 +0000

    typo download_unzip #30

--- intermediates/exo3_final.py
+++ intermediates/exo3_final.py
@@ -1,8 +1,5 @@
-import pandas as pd
 import geopandas as gpd
-import plotly.express as px
-from plotnine import ggplot, geom_line, aes
-
+import pandas as pd
 import src.import_data as sid
 from src.create_data_list import create_data_list
 
@@ -10,32 +7,27 @@ from src.create_data_list import create_data_list
 urls = create_data_list("./sources.yml")
 
 
-pax_apt_all = sid.import_airport_data(urls['airports'].values())
-pax_cie_all = sid.import_airport_data(urls['compagnies'].values())
-pax_lsn_all = sid.import_airport_data(urls['liaisons'].values())
+pax_apt_all = sid.import_airport_data(sid.download_and_unzip(urls["airports"]))
+pax_cie_all = sid.import_airport_data(sid.download_and_unzip(urls["compagnies"]))
+pax_lsn_all = sid.import_airport_data(sid.download_and_unzip(urls["liaisons"]))
 
 
-airports_location = gpd.read_file(
-    urls['geojson']['airport']
-)
+airports_location = gpd.read_file(urls["geojson"]["airport"])
 
 
-liste_aeroports = pax_apt_all['apt'].unique()
+liste_aeroports = pax_apt_all["apt"].unique()
 default_airport = liste_aeroports[0]
 
 
 # OBJETS NECESSAIRES A L'APPLICATION ------------------------
 
-pax_apt_all['trafic'] = pax_apt_all['apt_pax_dep'] + \
-  pax_apt_all['apt_pax_tr'] + \
-  pax_apt_all['apt_pax_arr']
-
-trafic_aeroports = (
-  pax_apt_all
-  .loc[pax_apt_all['apt'] == default_airport]
+pax_apt_all["trafic"] = (
+    pax_apt_all["apt_pax_dep"] + pax_apt_all["apt_pax_tr"] + pax_apt_all["apt_pax_arr"]
 )
-trafic_aeroports['date'] = pd.to_datetime(
-  trafic_aeroports['anmois'] + '01', format='%Y%m%d'
+
+trafic_aeroports = pax_apt_all.loc[pax_apt_all["apt"] == default_airport]
+trafic_aeroports["date"] = pd.to_datetime(
+    trafic_aeroports["anmois"] + "01", format="%Y%m%d"
 )
 
 
